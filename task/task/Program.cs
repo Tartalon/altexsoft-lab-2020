35d3a86d1db340cb63c426552def47603fbc0e0d
Vladimir <tartalon23@gmail.com>
2020-07-27 17:02:15 +0300

    get the file

---
+++ task/task/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Укажите путь к файлу");
+            string fileName = Console.ReadLine();
+            string fileText = File.ReadAllText(fileName);
+            Console.WriteLine(fileText);
+            Console.WriteLine("Укажите слово, символ");
+            string word = Console.ReadLine();
+            string changeText = fileText.Replace(word, "");
+            Console.WriteLine(changeText);
+            Console.ReadKey();
+
+        }
+    }
+}
